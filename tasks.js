2b4841665b3322e22ef46536543dc698783a260e
MohammadMehdi Moazedi <mehdimoazedy@gmail.com>
2024-11-22 12:49:54 +0330

    Update tasks.js

---
+++ tasks.js
@@ -0,0 +1,8 @@
+// State of the app
+const tasks = ['Walk the dog', 'Water the plants', 'Sand the chairs']
+
+
+// HTML element references
+const newTask = document.getElementById('newTask')
+const btn = document.getElementById('btn')
+const tasaksList = document.getElementById('tasksList')
