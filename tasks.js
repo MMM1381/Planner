2e64925a66b763282186e6fb3b5e27fc5fdd7eef
MohammadMehdi Moazedi <mehdimoazedy@gmail.com>
2024-11-22 12:57:12 +0330

    adding javascript
    now for the DOM

--- tasks.js
+++ tasks.js
@@ -5,4 +5,9 @@ const tasks = ['Walk the dog', 'Water the plants', 'Sand the chairs']
 // HTML element references
 const newTask = document.getElementById('newTask')
 const btn = document.getElementById('btn')
-const tasaksList = document.getElementById('tasksList')
+const tasksList = document.getElementById('tasksList')
+
+tasks.forEach(task => {
+    console.log(task)
+    tasksList.innerHTML += "<li> ${task} </li>";
+});
